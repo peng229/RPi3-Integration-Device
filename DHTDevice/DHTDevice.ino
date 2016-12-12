f8f101db3e41b65a86d5daf2d76ec94531f51e09
Joakim Hellberg <joakim.hellberg@sigma.se>
2016-12-12 11:28:50 +0100

    Removed dht libraries, using libraries provided by arduino IDE. Updated DHTDevice.ino

--- DHTDevice/DHTDevice.ino
+++ DHTDevice/DHTDevice.ino
@@ -1,24 +1,24 @@
-#include </DHT/dht.h>
-#define dht_dpin A0
+#include <dht.h>
+#define DHT11PIN A0
 dht DHT;
 
 String serialNumber = "11:11:11:11:11:11";
 
 void setup()
 {
-	Serial.begin(9600);
-	delay(1000);
+  Serial.begin(9600);
+  delay(1000);
 }
 
 void loop()
 {
-	DHT.read11(dht_dpin);
+  DHT.read11(DHT11PIN);
  
   Serial.print("ID:");
   Serial.print(serialNumber);
-	Serial.print(";");  
-	Serial.print("TEMP:");
-	Serial.print(DHT.temperature);
+  Serial.print(";");  
+  Serial.print("TEMP:");
+  Serial.print(DHT.temperature);
   Serial.println(";");  
   
   Serial.flush();  
@@ -26,10 +26,10 @@ void loop()
   Serial.print("ID:");
   Serial.print(serialNumber);
   Serial.print(";");    
-	Serial.print("HUM:");
-	Serial.print(DHT.humidity);
+  Serial.print("HUM:");
+  Serial.print(DHT.humidity);
   Serial.println(";");
   
   Serial.flush();  
-	delay(1000);
+  delay(1000);
 }
